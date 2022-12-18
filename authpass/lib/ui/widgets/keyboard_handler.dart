6ea1a9f7dadfd2acff94f50159fa336227b9cfb8
Kenneth Liang <kennethliang0@gmail.com>
2022-12-17 22:47:21 -0500

    Update and fix versions

--- authpass/lib/ui/widgets/keyboard_handler.dart
+++ authpass/lib/ui/widgets/keyboard_handler.dart
@@ -139,9 +139,10 @@ class _KeyboardHandlerState extends State<KeyboardHandler> {
         return null;
       })
     };
-    return Shortcuts(
-      manager: LoggingShortcutManager(),
-      shortcuts: shortcuts,
+    return Shortcuts.manager(
+      manager: LoggingShortcutManager(
+        shortcuts: shortcuts,
+      ),
       child: Provider<KeyboardShortcutEvents>.value(
         value: _keyboardShortcutEvents,
         child: Actions(
@@ -209,6 +210,11 @@ class IntentActionRegistration {
 
 /// A ShortcutManager that logs all keys that it handles.
 class LoggingShortcutManager extends ShortcutManager {
+  LoggingShortcutManager({
+    Map<ShortcutActivator, Intent> shortcuts =
+        const <ShortcutActivator, Intent>{},
+  }) : super(shortcuts: shortcuts);
+
   @override
   KeyEventResult handleKeypress(BuildContext context, RawKeyEvent event,
       {LogicalKeySet? keysPressed}) {
